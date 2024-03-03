9785c095c6e1ca7cf4fe59a33a8f1dad450ad20b
vladislav-andriushchenko <vladitheboy@gmail.com>
2024-03-03 10:32:32 -0800

    - added new classes

---
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -0,0 +1,55 @@
+package manager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ApplicationManager {
+
+    protected WebDriver driver;
+    private LoginHelper session;
+    private GroupHelper groups;
+
+    public void init(String browser) {
+        if (driver == null) {
+            if ("firefox".equals(browser)) {
+                driver = new FirefoxDriver();
+            } else if ("chrome".equals(browser)) {
+                driver = new ChromeDriver();
+            } else {
+                throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
+            }
+
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+            driver.get("http://localhost/addressbook/");
+            driver.manage().window().setSize(new Dimension(945, 1020));
+            session().login("admin", "secret");
+        }
+    }
+
+    public LoginHelper session() {
+        if (session == null) {
+            session = new LoginHelper(this);
+        }
+        return session;
+    }
+
+    public GroupHelper groups() {
+        if (groups == null) {
+            groups = new GroupHelper(this);
+        }
+        return groups;
+    }
+
+    public boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException exception) {
+            return false;
+        }
+    }
+}
