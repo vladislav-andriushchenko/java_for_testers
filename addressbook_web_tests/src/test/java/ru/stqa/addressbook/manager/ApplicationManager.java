4070b99308e7a5b01c0131e47ff7ec04b4383f6b
vladislav-andriushchenko <vladitheboy@gmail.com>
2024-03-19 12:26:07 -0700

    - added config file

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/manager/ApplicationManager.java
@@ -7,14 +7,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.Properties;
+
 public class ApplicationManager {
 
     protected WebDriver driver;
     private LoginHelper session;
     private GroupHelper groups;
     private ContactHelper contacts;
+    private Properties properties;
 
-    public void init(String browser) {
+    public void init(String browser, Properties properties) {
+        this.properties = properties;
         if (driver == null) {
             if ("firefox".equals(browser)) {
                 driver = new FirefoxDriver();
@@ -25,9 +29,9 @@ public class ApplicationManager {
             }
 
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get("http://localhost/addressbook/");
+            driver.get(properties.getProperty("web.baseUrl"));
             driver.manage().window().setSize(new Dimension(945, 1020));
-            session().login("admin", "secret");
+            session().login(properties.getProperty("web.username"), properties.getProperty("web.password"));
         }
     }
 
