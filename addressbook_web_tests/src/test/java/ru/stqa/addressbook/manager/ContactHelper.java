2016b18d26f82daaf62e3c95ab55977c8762437d
vladislav-andriushchenko <vladitheboy@gmail.com>
2024-04-21 12:54:47 -0700

    - Fixed bugs

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/manager/ContactHelper.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/manager/ContactHelper.java
@@ -126,7 +126,6 @@ public class ContactHelper extends HelperBase {
 
     public void removeAllContacts() {
         openHomePage();
-        selectAllOption();
         selectAllContacts();
         removeSelectedContacts();
     }
