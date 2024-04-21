7c431f11e44cc5f820f8c434a1c83925a875fb21
vladislav-andriushchenko <vladitheboy@gmail.com>
2024-04-21 11:43:27 -0700

    - Fixed bugs

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactModificationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactModificationTests.java
@@ -1,9 +1,9 @@
 package ru.stqa.addressbook.tests;
 
-import ru.stqa.addressbook.common.CommonFunctions;
-import ru.stqa.addressbook.model.ContactData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import ru.stqa.addressbook.common.CommonFunctions;
+import ru.stqa.addressbook.model.ContactData;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -14,7 +14,13 @@ public class ContactModificationTests extends TestBase {
     @Test
     void canModifyContact() {
         if (app.hmb().getContactCount() == 0) {
-            app.contacts().createContact(new ContactData("", "firstName", "lastName", "Address", "14882280", "test@test.test", "", "", "", "", "", ""));
+            app.contacts().createContact(new ContactData()
+                    .withFirstName(CommonFunctions.randomString(5))
+                    .withLastName(CommonFunctions.randomString(5))
+                    .withAddress(CommonFunctions.randomString(5))
+                    .withMobile(CommonFunctions.randomString(5))
+                    .withEmail(CommonFunctions.randomString(5))
+                    .withPhoto(randomFile("src/test/resources/images")));
         }
         var oldContacts = app.hmb().getContactList();
         var rnd = new Random();
@@ -29,6 +35,9 @@ public class ContactModificationTests extends TestBase {
         app.contacts().modifyContact(oldContacts.get(index), testData);
         var newContacts = app.hmb().getContactList();
         var expectedList = new ArrayList<>(oldContacts);
+        expectedList.set(index, testData
+                .withId(oldContacts.get(index).id()).withPhoto(""));
+
         Assertions.assertEquals(Set.copyOf(newContacts), Set.copyOf(expectedList));
     }
 }
